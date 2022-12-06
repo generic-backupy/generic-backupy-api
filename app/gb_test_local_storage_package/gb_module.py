dbee75cba291559345ca591430b6c837795b6555
Martin Rader <m1rader@edu.aau.at>
2022-12-06 10:07:48 +0100

    issue-1: finish storage function of local storage test module

--- app/gb_test_local_storage_package/gb_module.py
+++ app/gb_test_local_storage_package/gb_module.py
@@ -1,33 +1,42 @@
 from gb_module.gb_module.core.storage_module import StorageModule
 from gb_module.gb_module.core.backup_result import BackupResult
 from gb_module.gb_module.core.storage_result import StorageResult
-import time
 from pathlib import Path
+import shutil
 
 class GBModule(StorageModule):
     def save_to_storage(self, backup_result: BackupResult):
         # check if we have a path
+        self.log(f"check path ...")
         path = self.get_param_with_name('path')
         if not path:
             return StorageResult.with_error("No path was provided!")
 
-        # create file name and full path to that file
+        # create file name and full path for that file
+        self.log(f"create paths ...")
         path = Path(path)
         backup_file_name = self.get_backup_file_name()
         full_path = Path(path.absolute()).joinpath(backup_file_name)
 
         # save raw_backup to file
         if backup_result.raw_backup:
+            self.log(f"save raw backup to {full_path.absolute()} ...")
             try:
                 with open(full_path.absolute(), 'w') as file:
                     file.write(backup_result.raw_backup)
             except Exception as exception:
+                self.log(f"error at storing process: {exception}")
                 return StorageResult.with_error(f"error at storing process: {exception}")
-        else:
-            # TODO: implement datatransfer (blockwise if we need it to do per python code)
-            pass
+        else: # use the temporal saved file (for big files) and copy it to the destination
+            self.log(f"save temp file from {backup_result.backup_temp_location} to {full_path.absolute()} ...")
+            try:
+                shutil.copyfile(backup_result.backup_temp_location, full_path.absolute())
+            except Exception as exception:
+                self.log(f"error at storing process: {exception}")
+                return StorageResult.with_error(f"error at storing process: {exception}")
 
+        # return the StorageResult
+        self.log(f"backup was successful")
         return StorageResult(path=f"{full_path.absolute()}",
-                             additional_parameters_dict={"token": "dfdfdf"},
                              output="Backup successfully stored"
                              )
--- app/gb_test_local_storage_package/tests/general.py
+++ app/gb_test_local_storage_package/tests/general.py
@@ -11,26 +11,28 @@ import shutil
 class GeneralTest(TestCase):
     module: GBModule = None
     backup_location = "backup"
+    backup_location_temp = "backup/temp"
+    backup_location_temp_test_file = "backup/temp/test.bk"
 
     @classmethod
     def setUpClass(cls):
         if not os.path.exists(GeneralTest.backup_location):
             os.mkdir(GeneralTest.backup_location)
+        if not os.path.exists(GeneralTest.backup_location_temp):
+            os.mkdir(GeneralTest.backup_location_temp)
+        with open(GeneralTest.backup_location_temp_test_file, "w") as file:
+            file.write("hello backup from temp :)")
 
     @classmethod
     def tearDownClass(cls):
         if os.path.exists(GeneralTest.backup_location):
-            #shutil.rmtree(GeneralTest.backup_location, ignore_errors=True)
-            pass
+            shutil.rmtree(GeneralTest.backup_location, ignore_errors=True)
 
     def setUp(self):
-        # backup locations
-        self.backup_location_temp = "backup/temp"
-
         self.module = ModuleTestUtil.create_storage_module(GBModule, self.backup_location)
         self.backup_raw = BackupResultTestUtil.create_result_raw_backup()
         self.backup_temp_location_wrong = BackupResultTestUtil.create_result_backup_temp_location("/wrong-location")
-        self.backup_temp_location_wrong = BackupResultTestUtil.create_result_backup_temp_location(self.backup_location_temp)
+        self.backup_temp_location_correct = BackupResultTestUtil.create_result_backup_temp_location(GeneralTest.backup_location_temp_test_file)
 
     def test_module_creation(self):
         self.assertIsNotNone(self.module, "Object is not allowed to be None")
@@ -40,11 +42,16 @@ class GeneralTest(TestCase):
         response = self.module.save_to_storage(self.backup_raw)
         self.assertIsNotNone(response.error, "Response should contains a no path error")
 
-    def test_save_to_storage(self):
-        print(f"our path: {os.getcwd()}\n{os.listdir()}")
+    def test_save_backup_raw_to_storage(self):
         response = self.module.save_to_storage(self.backup_raw)
         self.assertIsNone(response.error, "Storage Error")
         self.assertIsNotNone(response)
-        print(f"path: {response.path}")
         self.assertTrue(os.path.exists(response.path), "Response Path doesn't exist")
 
+    def test_save_temp_backup_wrong_location_to_storage(self):
+        response = self.module.save_to_storage(self.backup_temp_location_wrong)
+        self.assertIsNotNone(response.error, "Storage Error")
+
+    def test_save_temp_backup_correct_location_to_storage(self):
+        response = self.module.save_to_storage(self.backup_temp_location_correct)
+        self.assertIsNone(response.error, "Storage Error")
