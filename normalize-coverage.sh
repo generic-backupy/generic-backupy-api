2152e9fa95c2d312ab1bcb8897088a655c32ec7c
Martin Rader <m1rader@edu.aau.at>
2022-12-14 21:13:19 +0100

    issue-6: fix the sonarcloud coverage path problem

--- normalize-coverage.sh
+++ normalize-coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' -e 's#/code#/app#g' app/coverage.xml
+  sed -i '' -e 's#/code#app#g' app/coverage.xml
 else
-  sed -i -e 's#/code#/app#g' app/coverage.xml
+  sed -i -e 's#/code#app#g' app/coverage.xml
 fi
