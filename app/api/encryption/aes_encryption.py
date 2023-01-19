3d9f4349fd278278697eecc335dd07b08b4402fb
ghoneimm <mghoneim96@gmail.com>
2023-01-19 23:42:51 +0100

    update encryption module

--- app/api/encryption/aes_encryption.py
+++ app/api/encryption/aes_encryption.py
@@ -3,29 +3,33 @@ from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 
 
-def encrypt(data: str) -> bytes:
-    # generate a random key
-    key = get_random_bytes(32)
+class EncryptionAES():
+    def create_key() -> bytes:
+        return get_random_bytes(32)
 
-    # generate an initialization vector
-    iv = get_random_bytes(16)
+    
+    def create_iv() -> bytes:
+        return get_random_bytes(16)
 
-    data = bytes(data, 'utf-8')
+  
+    def encrypt(data: str, key: bytes, iv: bytes) -> bytes:
 
-    # create AES cipher object
-    cipher = AES.new(key, AES.MODE_CBC, iv)
+        data = bytes(data, 'utf-8')
 
-    padded_data = pad(data, AES.block_size, style='pkcs7')
-    encrypted = cipher.encrypt(padded_data)
+        # create AES cipher object
+        cipher = AES.new(key, AES.MODE_CBC, iv)
 
-    return encrypted, key, iv
+        padded_data = pad(data, AES.block_size, style='pkcs7')
+        encrypted = cipher.encrypt(padded_data)
+
+        return encrypted
 
 
-def decrypt(encrypted: bytes, key: bytes, iv: bytes) -> str:
+    def decrypt(encrypted: bytes, key: bytes, iv: bytes) -> str:
 
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    decrypted = cipher.decrypt(encrypted)
-    unpadded_data = unpad(decrypted, AES.block_size, style='pkcs7')
-    original_data = unpadded_data.decode('utf-8')
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted = cipher.decrypt(encrypted)
+        unpadded_data = unpad(decrypted, AES.block_size, style='pkcs7')
+        original_data = unpadded_data.decode('utf-8')
 
-    return original_data
+        return original_data
