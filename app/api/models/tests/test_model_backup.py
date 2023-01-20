9531215aababc6635be7d47f2d24ef4be192df25
KarolWes <karol.wesolowski01@gmail.com>
2023-01-20 18:06:06 +0100

    test models: user

--- app/api/models/tests/test_model_backup.py
+++ app/api/models/tests/test_model_backup.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from api.models import Backup
+from django.db import IntegrityError
 
 
 # Create your tests here.
@@ -10,16 +11,11 @@ class TestModelBackup(TestCase):
         backups = Backup.objects.all()
         backups.delete()
 
-    def test_backup_empty_required_fields(self):
-        b = None
-        try:
+    def test_empty_required_fields(self):
+        with self.assertRaises(IntegrityError):
             b = Backup.objects.create(name=None)
-        except:
-            pass
-        finally:
-            self.assertIsNone(b, "Non-null constraint violated")
 
-    def test_backup_create_required_fields_only(self):
+    def test_create_required_fields_only(self):
         b = Backup.objects.create(name="name")
         backups = Backup.objects.all()
         self.assertEqual(len(backups), 1, "Object not added to db")
@@ -27,7 +23,7 @@ class TestModelBackup(TestCase):
         self.assertEqual(backups[0].name, "name", "Object has wrong name, failed to save properly")
         self.assertIsNone(backups[0].original_file_name, "Default value for not required field is not empty")
 
-    def test_backup_create_more_fields(self):
+    def test_create_more_fields(self):
         b = Backup.objects.create(name="name", original_file_name="file", path="path")
         backups = Backup.objects.all()
         self.assertEqual(len(backups), 1, "Object not added to db")
@@ -38,4 +34,8 @@ class TestModelBackup(TestCase):
         self.assertEqual(backups[0].path, "path", "Object has wrong content in field 'path'")
         self.assertIsNone(backups[0].description, "Default value for not required field is not empty")
 
-
+    def test_delete(self):
+        b = Backup.objects.create(name="name")
+        backups = Backup.objects.all()
+        backups.delete()
+        self.assertEqual(len(backups), 0, "Error while deleting")
