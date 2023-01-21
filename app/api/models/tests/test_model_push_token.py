59985636de0be32b5ed4a842216bff7aa28d27a9
KarolWes <karol.wesolowski01@gmail.com>
2023-01-21 18:20:07 +0100

    test models: push token

---
+++ app/api/models/tests/test_model_push_token.py
@@ -0,0 +1,42 @@
+from django.test import TestCase, TransactionTestCase
+
+from api.models import PushToken, User
+from django.db import IntegrityError
+from knox.models import AuthToken
+
+
+def create_user_auth_token():
+    u = User.objects.create_user(username="user")
+    auth = AuthToken.objects.create(u)
+    return auth[0]
+
+
+class TestModelPushToken(TestCase):
+    def setUp(self):
+        db = PushToken.objects.all()
+        db.delete()
+
+    def test_create_empty_required_fields(self):
+        with self.assertRaises(IntegrityError, msg="Non-null constraint violated"):
+            PushToken.objects.create(key=None, auth_token=None)
+
+    def test_create_required_fields_only(self):
+        auth = create_user_auth_token()
+        PushToken.objects.create(key="key", auth_token=auth)
+        db = PushToken.objects.all()
+        self.assertEqual(len(db), 1, "Object not added to db")
+        self.assertEqual(db[0].key, "key", "Error in field 'key'")
+
+
+class TestModelPushTokenTrans(TransactionTestCase):
+
+    def test_uniqueness(self):
+        auth = create_user_auth_token()
+        PushToken.objects.create(key="key", auth_token=auth)
+        db = PushToken.objects.all()
+        self.assertEqual(len(db), 1, "Object not added to db")
+        PushToken.objects.create(key="other_key", auth_token=auth)
+        db = PushToken.objects.all()
+        self.assertEqual(len(db), 2, "Second object not added to db")
+        with self.assertRaises(IntegrityError, msg="Uniqueness constraints violated"):
+            PushToken.objects.create(key="key", auth_token=auth)
