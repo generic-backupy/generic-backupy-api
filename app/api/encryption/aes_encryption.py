fcee4f5a14d2cbc3f0b33bc8fcdae03a2c8e4f99
ghoneimm <mghoneim96@gmail.com>
2023-01-21 10:37:04 +0100

    update encryption

--- app/api/encryption/aes_encryption.py
+++ app/api/encryption/aes_encryption.py
@@ -4,12 +4,8 @@ from Crypto.Random import get_random_bytes
 
 
 class EncryptionAES():
-    def create_key() -> bytes:
+    def create_key(self) -> bytes:
         return get_random_bytes(32)
-
-    
-    def create_iv() -> bytes:
-        return get_random_bytes(16)
 
   
     def encrypt(data: str, key: bytes, iv: bytes) -> bytes:
@@ -17,7 +13,7 @@ class EncryptionAES():
         data = bytes(data, 'utf-8')
 
         # create AES cipher object
-        cipher = AES.new(key, AES.MODE_CBC, iv)
+        cipher = AES.new(key, AES.MODE_GCM)
 
         padded_data = pad(data, AES.block_size, style='pkcs7')
         encrypted = cipher.encrypt(padded_data)
@@ -25,9 +21,9 @@ class EncryptionAES():
         return encrypted
 
 
-    def decrypt(encrypted: bytes, key: bytes, iv: bytes) -> str:
+    def decrypt(encrypted: bytes, key: bytes) -> str:
 
-        cipher = AES.new(key, AES.MODE_CBC, iv)
+        cipher = AES.new(key, AES.MODE_GCM)
         decrypted = cipher.decrypt(encrypted)
         unpadded_data = unpad(decrypted, AES.block_size, style='pkcs7')
         original_data = unpadded_data.decode('utf-8')
