fea1ebbdf3bfc5a9527973da991d0fa5ceada5f2
ghoneimm <mghoneim96@gmail.com>
2023-01-21 12:44:19 +0100

    add tests: encryption

--- app/api/encryption/aes_encryption.py
+++ app/api/encryption/aes_encryption.py
@@ -7,23 +7,22 @@ class EncryptionAES():
     def create_key(self) -> bytes:
         return get_random_bytes(32)
 
+    def create_iv(self) -> bytes:
+        return get_random_bytes(16)
   
-    def encrypt(data: str, key: bytes, iv: bytes) -> bytes:
-
+    def encrypt(self, data: str, key: bytes, iv: bytes) -> bytes:
         data = bytes(data, 'utf-8')
-
         # create AES cipher object
-        cipher = AES.new(key, AES.MODE_GCM)
-
+        cipher = AES.new(key, AES.MODE_GCM, iv)
         padded_data = pad(data, AES.block_size, style='pkcs7')
         encrypted = cipher.encrypt(padded_data)
 
         return encrypted
 
 
-    def decrypt(encrypted: bytes, key: bytes) -> str:
+    def decrypt(self, encrypted: bytes, key: bytes, iv: bytes) -> str:
 
-        cipher = AES.new(key, AES.MODE_GCM)
+        cipher = AES.new(key, AES.MODE_GCM, iv)
         decrypted = cipher.decrypt(encrypted)
         unpadded_data = unpad(decrypted, AES.block_size, style='pkcs7')
         original_data = unpadded_data.decode('utf-8')
---
+++ app/api/encryption/tests/test_encryption.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from api.encryption import EncryptionAES
+
+class TestEncryption(TestCase):
+
+    def setUp(self):
+        self.encryption_aes = EncryptionAES()
+        self.test_data = "This is a test secret"
+        self.test_key = self.encryption_aes.create_key()
+        self.test_iv = self.encryption_aes.create_iv()   
+    
+    def test_key_generation(self):
+        key = self.encryption_aes.create_key()
+        self.assertEqual(len(key), 32)
+    
+    def test_key_generation(self):
+        key = self.encryption_aes.create_key()
+        self.assertEqual(len(key), 16)
+    
+    def test_encryption_correct(self):
+        encrypted = self.encryption_aes.encrypt(self.test_data, self.test_key, self.test_iv)
+        decrypted = self.encryption_aes.decrypt(encrypted, self.test_key, self.test_iv)
+        self.assertEqual(decrypted, self.test_data)
+
+    def test_encrypt_with_wrong_key(self):
+        encrypted = self.encryption_aes.encrypt(self.test_data, self.test_key, self.test_iv)
+        new_key = self.encryption_aes.create_key()
+        decrypted = self.encryption_aes.decrypt(encrypted, new_key, self.test_iv)
+        self.assertNotEqual(decrypted, self.test_data)
