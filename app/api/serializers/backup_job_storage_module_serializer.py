58071362f68647b446a1079bb13e8ccff23bc449
KarolWes <karol.wesolowski01@gmail.com>
2023-01-24 12:10:50 +0100

    retrieve secrets and parameters lists from backupjobstoragemodule + refactoring

--- app/api/serializers/backup_job_storage_module_serializer.py
+++ app/api/serializers/backup_job_storage_module_serializer.py
@@ -2,32 +2,57 @@ from rest_framework import serializers
 
 from ..models import BackupJobStorageModule
 
-class BackupJobStorageModulePostSerializer(serializers.ModelSerializer):
+
+class BackupJobStorageModuleSerializer(serializers.ModelSerializer):
+    secrets = serializers.SerializerMethodField('get_list_of_secrets')
+    parameters = serializers.SerializerMethodField('get_list_of_parameters')
+
+    def get_list_of_secrets(self, current_object: BackupJobStorageModule):
+        secrets = list(current_object.secrets.all())
+        ans = []
+        for s in secrets:
+            ans.append([s.id, s.name])
+        return ans
+
+    def get_list_of_parameters(self, current_object: BackupJobStorageModule):
+        params = list(current_object.parameters.all())
+        ans = []
+        for p in params:
+            ans.append([p.id, p.name])
+
+        return ans
+
     class Meta:
         model = BackupJobStorageModule
-        fields = ('id', 'backup_job', 'storage_module')
+        fields = ('id', 'backup_job', 'storage_module', 'secrets', 'parameters')
 
-class BackupJobStorageModuleRetrieveSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BackupJobStorageModule
-        fields = ('id', 'backup_job', 'storage_module')
 
-class BackupJobStorageModuleListSerializer(serializers.ModelSerializer):
+class BackupJobStorageModulePostSerializer(BackupJobStorageModuleSerializer):
+    pass
+
+
+class BackupJobStorageModuleRetrieveSerializer(BackupJobStorageModuleSerializer):
+    pass
+
+
+class BackupJobStorageModuleBaseSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField('get_name')
+
     def get_name(self, current_object: BackupJobStorageModule):
         return current_object.backup_job.name
+
     class Meta:
         model = BackupJobStorageModule
         fields = ('id', 'name')
 
-class BackupJobStorageModuleGetShortSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField('get_name')
 
-    def get_name(self, current_object: BackupJobStorageModule):
-        return current_object.backup_job.name
-    class Meta:
-        model = BackupJobStorageModule
-        fields = ('id', 'name')
+class BackupJobStorageModuleListSerializer(BackupJobStorageModuleBaseSerializer):
+    pass
+
+
+class BackupJobStorageModuleGetShortSerializer(BackupJobStorageModuleBaseSerializer):
+    pass
+
 
 class BackupJobStorageModuleOnlyIdSerializer(serializers.ModelSerializer):
     class Meta:
