53675f58a7b2d2e2ee42e85eddf7a843e7be212e
ghoneimm <mghoneim96@gmail.com>
2023-01-25 18:37:11 +0100

    tests for system serializer and user serializer

--- app/api/testing/serializers/test_user_serializer.py
+++ app/api/testing/serializers/test_user_serializer.py
@@ -1,31 +1,62 @@
 from api.models import User
-from api.serializers import (UserGetShortSerializer, UserListSerializer,
-                             UserOnlyIdSerializer, UserPostSerializer,
-                             UserRetrieveSerializer, UserSerializer,
-                             UserShortListSerializer, UserUpdateSerializer)
+from api.serializers import (UserGetShortSerializer, UserOnlyIdSerializer,
+                             UserPostSerializer, UserRetrieveSerializer,
+                             UserSerializer, UserUpdateSerializer,
+                             UserListSerializer)
 from django.test import TestCase
 
 
 class TestUserSerializer(TestCase):
     def setUp(self):
         self.user = User.objects.create(
-            username='test_user',
-            first_name='max',
-            last_name='mustermann',
-            email='test@user.com',
-            password='testpassword'
+            username = 'test_user',
+            first_name = 'max',
+            last_name = 'mustermann',
+            email = 'test@user.com',
         )
 
     def test_user_serializer(self):
         serializer = UserSerializer(self.user)
+        self.assertTrue(serializer.is_valid())
         self.assertEqual(serializer.data, {
                          'id': self.user.pk, 'username': 'test_user'})
 
     def test_user_id_only_serializer(self):
         serializer = UserOnlyIdSerializer(self.user)
+        self.assertTrue(serializer.is_valid())
         self.assertEqual(serializer.data, {'id': self.user.pk})
 
     def test_user_post_serializer(self):
         serializer = UserPostSerializer(self.user)
+        self.assertTrue(serializer.is_valid())
         self.assertEqual(serializer.data, {
-                         'id': self.user.pk, 'first_name': 'max', 'last_name': 'mustermann', 'email': 'test@user.com'})
+                         'id': self.user.pk, 'first_name': 'max', 'last_name': 'mustermann',
+                          'email': 'test@user.com'})
+
+    def test_user_update_serializer(self):
+        serializer = UserUpdateSerializer(self.user)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.data, {
+                         'id': self.user.pk,  'first_name': 'max', 'last_name': 'mustermann',
+                          'email': 'test@user.com'})
+
+    def test_user_retrieve_serializer(self):
+        serializer = UserRetrieveSerializer(self.user)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.data['username'], 'test_user')
+        self.assertEqual(serializer.data['first_name'], 'max')
+        self.assertEqual(serializer.data['last_name'], 'mustermann')
+        self.assertEqual(serializer.data['email'], 'test@user.com')
+        self.assertNotEqual(serializer.data['email'], 'random@test.email',)
+
+    def test_user_get_short_serializer(self):
+        serializer = UserGetShortSerializer(self.user)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.data, {
+                         'id': self.user.pk, 'username': 'test_user'})
+
+    def test_user_list_serializer(self):
+        serializer = UserListSerializer(self.user)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.data['username'], 'test_user')
+
